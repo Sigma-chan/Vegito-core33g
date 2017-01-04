b13eeaa188be5657d8f0bf9b0f56fd69dfc70fcd
DreamStar001 <nikitsharma92@gmail.com>
2017-01-04 23:00:51 +0530

    Inital Push

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 SMP PREEMPT Wed Nov 12 22:21:08 EST 2014"
+#define LINUX_COMPILE_BY "koo"
+#define LINUX_COMPILE_HOST "koo-desktop"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
